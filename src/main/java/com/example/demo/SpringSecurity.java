7892c55e2ec165641a4047c38de16b02bd630e3b
imkenova <elena.imkenova@yandex.ru>
2024-01-17 01:20:22 +0300

    update SpringSecurity

--- src/main/java/com/example/demo/SpringSecurity.java
+++ src/main/java/com/example/demo/SpringSecurity.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -34,6 +35,7 @@ public class SpringSecurity {
                                 .requestMatchers("/register/**","/", "/new", "/save", "/edit/{id}", "/delete/{id}", "/blog", "/blog/new", "/blog/{id}/edit", "/blog/{id}/remove", "/blog/{id}", "/blog/save").permitAll()
                                 .requestMatchers("/users").hasRole("ADMIN")
                                 .requestMatchers("/users/**").hasRole("ADMIN")
+
                 ).formLogin(
                         form -> form
                                 .loginPage("/")
@@ -54,4 +56,10 @@ public class SpringSecurity {
                 .userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder());
     }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/images/**", "/js/src/**");
+    }
+
 }
